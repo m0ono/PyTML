7829be9008e0f0479ab9d00acd1e05c7ef8c33e0
m0ono <76524640+m0ono@users.noreply.github.com>
2021-12-03 12:56:21 +0300

    Create programmer.py

---
+++ programmer/programmer.py
@@ -0,0 +1,11 @@
+import os
+try:
+    import requests
+except:
+    os.system("pip install requests")
+print(requests.get('http://artii.herokuapp.com/make?text=programmer').text)
+name = input("What's you'r website name ? : ")
+bio = input("you'r bio ? : ")
+html = '<!DOCTYPE html>' + "\n" + '<html lang="en">' + "\n" + '<head>' + "\n" + '  <meta charset="UTF-8">' + "\n" + '  <meta http-equiv="X-UA-Compatible" content="IE=Edge">' + "\n" + '  <meta name="viewport" content="width=device-width, initial-scale=1">' + "\n" + '  <title></title>' + "\n" + '</head>'+ "\n" + '<body>' + "\n" + '  <div id="name">' + "\n" + f'    <h1>{name}</h1>' + "\n" + '    </div>' + "\n" + '  <div id="programmer">' + "\n" + '    <img src="https://content.techgig.com/photo/77087595/Guide-How-to-build-career-as-a-programmer-without-college-degree.jpg" height="200px" width="300px"></div>' + "\n" + '  <fieldset>' + "\n" + '    <legend>Biography</legend>' + "\n" +f'    {bio}' + "\n" +'  </fieldset>' + "\n" +'  <style>' + "\n" +'    body{' + "\n" +'      background: #E8E8E8;' + "\n" + '    }' + "\n" +'    *{' + "\n" +'      padding: 0;' + "\n" +'      margin: 0;' + "\n" +'    }' + "\n" +'    #name{' + "\n" +'     background:white;' + "\n" +'     height: 45px;' + "\n" + '     box-shadow: 0px 0px 0px 3px #D2D2D2;' + "\n" + '    }' + "\n" + '    #name h1{' + "\n" + "      font-family: 'Brush Script MT', cursive;" + "\n" + '      color: #FF7E04;' + "\n" + '      text-align: center;' + "\n" + '      font-size: 40px;' + "\n" +'    }' + "\n" + '    #programmer{' + "\n" + '      text-align: center;' + "\n" + '      padding-top: 20px;' + "\n" + '    }' + "\n" + '    #programmer img{' + "\n" +'      border-radius: 50%;' + "\n" + '    }' + "\n" + '    fieldset{' + "\n" + '      text-align: center;' + "\n" + '      overflow: hidden;' + "\n" + '      padding: 10px;' + "\n" + '      margin: 10px;' + "\n" + '      border: 5px solid gray;' + "\n" + '      color: #45018D;' + "\n" + '      font-size: 18px;' + "\n" + '      font-family: serif;' + "\n" + '    }' + "\n" + '    fieldset legend{' + "\n" + '      color: #FF7E04;' + "\n" + '      font-size: 25px;' + "\n" + '    }' + "\n" + '  </style>' + "\n" + '</body>' + "\n" + '</html>'
+with open("index.html", "a") as save:
+    save.write(html)
